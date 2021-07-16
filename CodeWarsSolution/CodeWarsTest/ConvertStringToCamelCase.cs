ff75a9bd406b12490f89e0d7af14c78615d30246
ChrisRSE <84640547+ChrisRSE@users.noreply.github.com>
2021-07-16 19:53:18 +0100

    Added Testcase

---
+++ CodeWarsSolution/CodeWarsTest/ConvertStringToCamelCase.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using CodeWars.Challenge_Solutions;
+using NUnit.Framework;
+
+namespace CodeWarsTest
+{
+    class ConvertStringToCamelCase
+    {
+        [TestCase("Convert_String_To_Camel_Case", "convertStringToCamelCase")]
+        [TestCase("Hello Again World", "helloAgainWorld")]
+        [TestCase("Its-Me-You're-Looking-For", "helloAgainWorld")]
+        public void TestStringConversion_ForCorrectOutput(String str, String expected)
+        {
+            Assert.That(ConvertStringToCamelCase.Solution(str), Is.EqualTo(expected));
+        }
+    }
+}
