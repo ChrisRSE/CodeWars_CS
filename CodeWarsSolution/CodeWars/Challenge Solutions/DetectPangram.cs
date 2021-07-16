14ae630d7605a69b3768bae07025d6d897e107ed
ChrisRSE <84640547+ChrisRSE@users.noreply.github.com>
2021-07-16 19:46:27 +0100

    Added DetectPangram Challenge - Working

---
+++ CodeWarsSolution/CodeWars/Challenge Solutions/DetectPangram.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars.Challenge_Solutions
+{
+    public class DetectPangram
+    {
+        public static bool Solution(String str)
+        {
+            return str.ToLower().Where(c => Char.IsLetter(c)).GroupBy(c => c).Count() == 26;
+        }
+    }
+}
