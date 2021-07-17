d3eaf3ff17c9475e6e697a18d60bca0087e0ec3f
ChrisRSE <84640547+ChrisRSE@users.noreply.github.com>
2021-07-17 09:08:45 +0100

    Added TakeATenMinWalk Solution - All Passed

---
+++ CodeWarsSolution/CodeWars/Challenge Solutions/TakeATenMinWalk.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars.Challenge_Solutions
+{
+    public class TakeATenMinWalk
+    {
+        public static bool Solution(String[] arr)
+        {
+            int yAxis = 0, xAxis = 0;
+            foreach (var str in arr)
+            {
+                switch (str)
+                {
+                    case ("n"):
+                        xAxis++;
+                        break;
+                    case ("s"):
+                        xAxis--;
+                        break;
+                    case ("e"):
+                        yAxis++;
+                        break;
+                    case ("w"):
+                        yAxis--;
+                        break;
+                }
+            }
+            if (yAxis == 0 && xAxis == 0 && arr.Length == 10)
+                return true;
+            else
+                return false;
+        }
+    }
+}
