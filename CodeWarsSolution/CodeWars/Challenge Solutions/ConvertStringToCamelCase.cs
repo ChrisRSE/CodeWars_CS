8bc3311191f525f41fc56c81fe2dcca9831049b8
ChrisRSE <84640547+ChrisRSE@users.noreply.github.com>
2021-07-17 08:42:00 +0100

    Added ConverStringToCamelCase challenge - All passed

---
+++ CodeWarsSolution/CodeWars/Challenge Solutions/ConvertStringToCamelCase.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars.Challenge_Solutions
+{
+    public class ConvertStringToCamelCase
+    {
+        public static string Solution(String str)
+        {
+            return Char.ToLowerInvariant(str[0]) + str.Substring(1).Replace("_", string.Empty).Replace(" ", string.Empty).Replace("-", string.Empty);
+        }
+    }
+}
