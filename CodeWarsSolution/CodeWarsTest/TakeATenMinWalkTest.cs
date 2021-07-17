590e8efef827088cd3196bd034d53de2e919a79e
ChrisRSE <84640547+ChrisRSE@users.noreply.github.com>
2021-07-17 09:08:35 +0100

    Added TakeATenMinWalk Testcases

---
+++ CodeWarsSolution/CodeWarsTest/TakeATenMinWalkTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using NUnit.Framework;
+using CodeWars.Challenge_Solutions;
+
+namespace CodeWarsTest
+{
+    class TakeATenMinWalkTest
+    {
+        [TestCase(new string[] {"n", "s", "n", "s", "n", "s", "n", "s", "n", "s"}, true)]
+        [TestCase(new string[] { "w", "e", "w", "e", "w", "e", "w", "e", "w", "e", "w", "e" }, false)]
+        [TestCase(new string[] { "w" }, false)]
+        [TestCase(new string[] { "n", "n", "n", "s", "n", "s", "n", "s", "n", "s" }, false)]
+
+        public static void TakeATenMinWalk_ReturnsCorrectly(string[] str, bool expected)
+        {
+            Assert.That(TakeATenMinWalk.Solution(str), Is.EqualTo(expected));
+        }
+    }
+}
