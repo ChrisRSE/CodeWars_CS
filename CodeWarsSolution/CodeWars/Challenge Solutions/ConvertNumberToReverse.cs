dc14093bb286c7bc8fefa2c9877953edcecbd57e
ChrisRSE <84640547+ChrisRSE@users.noreply.github.com>
2021-08-02 17:35:25 +0100

    Added NumberToReverse Testcase

---
+++ CodeWarsSolution/CodeWars/Challenge Solutions/ConvertNumberToReverse.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars.Challenge_Solutions
+{
+    public static class ConvertNumberToReverse
+    {
+        public static long[] Digitize(long n)
+        {
+            var stringArr = n.ToString().ToCharArray();
+            var longArr = new long[stringArr.Length];
+            int j = 0;
+            for(int i = longArr.Length - 1; i >= 0; i--)
+            {
+                longArr[j++] = stringArr[i] - 48;
+            }
+
+            return longArr;
+        }
+    }
+}
