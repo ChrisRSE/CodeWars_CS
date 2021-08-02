cdba0b2b2f916c7267670bbff26c012ade1e1cdc
ChrisRSE <84640547+ChrisRSE@users.noreply.github.com>
2021-08-02 17:35:35 +0100

    Added NumberToReverse solution

---
+++ CodeWarsSolution/CodeWarsTest/ConvertNumberToReverseTests.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CodeWars.Challenge_Solutions;
+
+namespace CodeWarsTests
+{
+    class ConvertNumberToReverseTests
+    {
+        [TestCase(35231, new long[] { 1, 3, 2, 5, 3 })]
+        [TestCase(990024, new long[] { 4, 2, 0, 0, 9, 9 })]
+        public void TestArrayReversesSuccessfully(long number, long[] expected)
+        {
+            Assert.That(ConvertNumberToReverse.Digitize(number), Is.EqualTo(expected));
+        }
+    }
+}
