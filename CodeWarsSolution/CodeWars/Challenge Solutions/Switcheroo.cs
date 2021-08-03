88022edd4312f278b49daea52297d9c3ea8fd9b8
ChrisRSE <84640547+ChrisRSE@users.noreply.github.com>
2021-08-03 18:38:39 +0100

    Added solution for Switcheroo

---
+++ CodeWarsSolution/CodeWars/Challenge Solutions/Switcheroo.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars.Challenge_Solutions
+{
+    public class Switcheroo
+    {
+        public static string SwitcherooMethod(string x)
+        {
+            ////Standard Solution
+            //string newString = "";
+            //foreach (var c in x)
+            //{
+            //    if (c == 'a')
+            //        newString += 'b';
+            //    else if (c == 'b')
+            //        newString += 'a';
+            //    else
+            //        newString += c;
+            //}
+            //return newString;
+
+            // One line
+            return x.Replace('a', 'x').Replace('b', 'a').Replace('x','b');
+        }
+    }
+}
---
+++ CodeWarsSolution/CodeWarsTest/SwitcherooTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CodeWars.Challenge_Solutions;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    class SwitcherooTests
+    {
+        [Test]
+        public void BasicTests()
+        {
+            Assert.AreEqual("bac", Switcheroo.SwitcherooMethod("abc"));
+            Assert.AreEqual("bbbacccabbb", Switcheroo.SwitcherooMethod("aaabcccbaaa"));
+            Assert.AreEqual("ccccc", Switcheroo.SwitcherooMethod("ccccc"));
+        }
+    }
+}
