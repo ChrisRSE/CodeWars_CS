5561659f133158d33da8f71b0adf691411c001fa
ChrisRSE <84640547+ChrisRSE@users.noreply.github.com>
2021-08-03 17:54:24 +0100

    Added CatYearsDogYears solution and tests

---
+++ CodeWarsSolution/CodeWars/Challenge Solutions/CatYearsDogYears.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars.Challenge_Solutions
+{
+    public class CatYearsDogYears
+    {
+        public static int[] humanYearsCatYearsDogYears(int y)
+        {
+            //// LONG SOLUTION
+            //int cat = 0;
+            //int dog = 0;
+
+            //if (y == 1)
+            //{
+            //    cat = 15;
+            //    dog = 15;
+            //}
+            //else if (y == 2)
+            //{
+            //    cat = 24;
+            //    dog = 24;
+            //}
+            //else
+            //{
+            //    cat = (y -2) * 4 +24;
+            //    dog = (y - 2) * 5 + 24;
+            //}
+            //return new int[] { y, cat, dog };
+
+            // One Line Solution
+            return y == 1 ? new int[] { y, 15, 15 } : y == 2 ?
+                new int[] { y, 24, 24 } : new int[] { y, (y - 2) * 4 + 24, (y - 2) * 5 + 24 };
+        }
+    }
+}
---
+++ CodeWarsSolution/CodeWarsTest/CatYearsDogYearsTests.cs
@@ -0,0 +1,31 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CodeWars.Challenge_Solutions;
+
+namespace CodeWarsTests
+{
+    class CatYearsDogYearsTests
+    {
+        [Test]
+        public void One()
+        {
+            Assert.AreEqual(new int[] { 1, 15, 15 }, CatYearsDogYears.humanYearsCatYearsDogYears(1));
+        }
+
+        [Test]
+        public void Two()
+        {
+            Assert.AreEqual(new int[] { 2, 24, 24 }, CatYearsDogYears.humanYearsCatYearsDogYears(2));
+        }
+
+        [Test]
+        public void Ten()
+        {
+            Assert.AreEqual(new int[] { 10, 56, 64 }, CatYearsDogYears.humanYearsCatYearsDogYears(10));
+        }
+    }
+}
